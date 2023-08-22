ab90559291824dc35859fa875ae4396ea9c27c01
onitsiky <hei.onitsiky@gmail.com>
2023-08-22 14:16:54 +0300

    feat: add configuration for integrating 2 databases

--- src/main/java/com/example/prog4/repository/cnaps/EmployeeCnapsRepository.java
+++ src/main/java/com/example/prog4/repository/cnaps/EmployeeCnapsRepository.java
@@ -1,11 +1,12 @@
-package com.example.prog4.repository;
+package com.example.prog4.repository.cnaps;
 
-import com.example.prog4.repository.entity.EmployeeCnaps;
+import com.example.prog4.repository.cnaps.entity.EmployeeCnaps;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-@ConfigurationProperties(prefix = "spring.datasource-db2")
 @Repository
+@Transactional("cnapsTransactionManager")
 public interface EmployeeCnapsRepository extends JpaRepository<EmployeeCnaps, String> {
 }
