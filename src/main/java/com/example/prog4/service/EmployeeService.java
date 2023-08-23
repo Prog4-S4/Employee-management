ead5bfc842e19b416e9b25e3efb6648b993f54f6
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-08-23 10:11:22 +0300

    chore:update implementation get all

--- src/main/java/com/example/prog4/service/EmployeeService.java
+++ src/main/java/com/example/prog4/service/EmployeeService.java
@@ -1,11 +1,10 @@
 package com.example.prog4.service;
 
+import com.example.prog4.model.Employee;
 import com.example.prog4.model.EmployeeFilter;
-import com.example.prog4.model.exception.NotFoundException;
 import com.example.prog4.repository.Repository;
 import com.example.prog4.repository.employee.EmployeeRepository;
 import com.example.prog4.repository.employee.dao.EmployeeManagerDao;
-import com.example.prog4.repository.employee.entity.Employee;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -29,16 +28,8 @@ public class EmployeeService {
     public List<Employee> getAll(EmployeeFilter filter) {
         Sort sort = Sort.by(filter.getOrderDirection(), filter.getOrderBy().toString());
         Pageable pageable = PageRequest.of(filter.getIntPage() - 1, filter.getIntPerPage(), sort);
-        return employeeManagerDao.findByCriteria(
-                filter.getLastName(),
-                filter.getFirstName(),
-                filter.getCountryCode(),
-                filter.getSex(),
-                filter.getPosition(),
-                filter.getEntrance(),
-                filter.getDeparture(),
-                pageable
-        );
+        return repository.getAll(filter,pageable);
+
     }
 
     public void saveOne(com.example.prog4.model.Employee employee) {
